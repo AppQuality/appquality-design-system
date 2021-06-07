4c22b0a843ab547d087a0ab6517d7a6bbee74927
Davide Bizzi <davide.bizzi@app-quality.com>
2021-06-07 13:22:38 +0200

    feat: Setup basic repo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import * as styles from './shared/styles';
+// import * as global from './shared/global';
+// import * as animation from './shared/animation';
+// import * as icons from './shared/icons';
+// 
+// export { styles, global, animation, icons };
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// export * from './Avatar';
+// export * from './Badge';
+// export * from './Button';
+// export * from './Icon';
+// export * from './Link';
