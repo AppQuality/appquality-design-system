f355dd1fd418744f183f9ed2c860761b5fa2e3dc
Davide Bizzi <davide.bizzi@app-quality.com>
2021-06-09 19:00:36 +0200

    fix: Typing on breakpoint list for container

--- src/shared/utils/containers.ts
+++ src/shared/utils/containers.ts
@@ -1,12 +1,13 @@
 import { DefaultTheme } from "styled-components";
 
 export const containers = (theme: DefaultTheme) => {
-  const responsiveWidth = {
+  const responsiveWidth: DefaultTheme["grid"]["breakpoints"] = {
     xxl: "1320px",
     xl: "1140px",
     lg: "960px",
     md: "720px",
     sm: "540px",
+    xs: "540px",
   };
 
   let style = `
@@ -21,17 +22,22 @@ export const containers = (theme: DefaultTheme) => {
 	
 	`;
 
-  style += Object.entries(theme.grid.breakpoints)
+  let breakpoints = Object.entries(theme.grid.breakpoints) as [
+    keyof DefaultTheme["grid"]["breakpoints"],
+    string
+  ][];
+  style += breakpoints
     .map(([key, value]) => {
-      if (!responsiveWidth.hasOwnProperty(key)) return "";
-
-      return `
-				@media (min-width: ${value}) {
-					.container {
-						max-width: ${responsiveWidth[key]}
-					}
-				}
-			`;
+      if (responsiveWidth.hasOwnProperty(key)) {
+        return `
+        @media (min-width: ${value}) {
+          .container {
+            max-width: ${responsiveWidth[key]}
+          }
+        }
+        `;
+      }
+      return "";
     })
     .join("");
   return style;
