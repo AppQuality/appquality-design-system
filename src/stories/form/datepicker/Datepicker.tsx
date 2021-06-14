3078391a369f789f9bb3ebcb60d74df1a444e93d
Davide Bizzi <davide.bizzi@app-quality.com>
2021-06-14 13:49:36 +0200

    rework(datepicker): Convert back to global style but include only once

--- src/stories/form/datepicker/Datepicker.tsx
+++ src/stories/form/datepicker/Datepicker.tsx
@@ -1,12 +1,10 @@
 import { Datepicker as MobiScrollDatePicker } from "@mobiscroll/react";
 import { DatepickerProps } from "./_types";
 import { DatepickerStyle } from "./_style";
-import { useRef } from "react";
 
 export const Datepicker = ({
   select,
   value,
-  id,
   minDate,
   maxDate,
   onChange,
@@ -15,27 +13,22 @@ export const Datepicker = ({
   setText = "Set",
   cancelText = "Cancel",
 }: DatepickerProps) => {
-  const datepickerContext = useRef(null);
-
   return (
     <div>
-      <DatepickerStyle id={id}>
-        <MobiScrollDatePicker
-          context={`#${id}`}
-          select={select}
-          min={minDate}
-          max={maxDate}
-          onChange={onChange}
-          placeholder={placeholder}
-          setText={setText}
-          cancelText={cancelText}
-          buttons={["cancel", "set"]}
-          theme="appquality"
-          controls={[control]}
-          themeVariant="light"
-        />
-        <div ref={datepickerContext} />
-      </DatepickerStyle>
+      <DatepickerStyle />
+      <MobiScrollDatePicker
+        select={select}
+        min={minDate}
+        max={maxDate}
+        onChange={onChange}
+        placeholder={placeholder}
+        setText={setText}
+        cancelText={cancelText}
+        buttons={["cancel", "set"]}
+        theme="appquality"
+        controls={[control]}
+        themeVariant="light"
+      />
     </div>
   );
 };
