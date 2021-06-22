723ba09b13fa0565a784821021168902da6afd29
iacopolea <iacopo.leardini@app-quality.com>
2021-06-22 14:36:39 +0200

    feat(modal): add onClose prop and use children for body

--- src/stories/modal/Modal.tsx
+++ src/stories/modal/Modal.tsx
@@ -5,22 +5,23 @@ import { X } from "react-bootstrap-icons";
 
 export const Modal: React.FC<ModalProps & BaseProps> = ({
   isOpen,
+  onClose,
   title,
-  content,
   footer,
+  children,
 }) => {
   return (
     <>
       {isOpen && (
-        <ModalOverlay onClick={() => alert("close modal?")}>
+        <ModalOverlay onClick={onClose}>
           <div className="modal">
             <div className="modal-header">
               <div className="modal-title">{title}</div>
-              <div className="modal-close" onClick={() => alert("close modal")}>
+              <div className="modal-close" onClick={onClose}>
                 <X />
               </div>
             </div>
-            <div className="modal-body">{content}</div>
+            <div className="modal-body">{children}</div>
             <div className="modal-footer">{footer}</div>
           </div>
         </ModalOverlay>
--- src/stories/modal/_types.ts
+++ src/stories/modal/_types.ts
@@ -4,4 +4,5 @@ export interface ModalProps {
   title?: React.ReactNode;
   content: React.ReactNode;
   footer?: React.ReactNode;
+  onClose: () => void;
 }
