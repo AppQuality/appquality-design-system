77f2dfe3da47a6d2a815c10c44ba140b889cf30d
iacopolea <iacopo.leardini@app-quality.com>
2021-06-22 14:50:47 +0200

    fix(modal): remove required prop content

--- src/stories/modal/_types.ts
+++ src/stories/modal/_types.ts
@@ -2,7 +2,6 @@ import * as React from "react";
 export interface ModalProps {
   isOpen: boolean;
   title?: React.ReactNode;
-  content: React.ReactNode;
   footer?: React.ReactNode;
   onClose: () => void;
 }
