c890766229ec8e02f9cb0710b7c07e7f8ff3a633
iacopolea <iacopo.leardini@app-quality.com>
2021-06-25 11:35:52 +0200

    chore(Modal): move types around

--- src/stories/modal/Modal.stories.tsx
+++ src/stories/modal/Modal.stories.tsx
@@ -1,7 +1,6 @@
 import { Story, Meta } from "@storybook/react";
 import { ThemeProvider } from "styled-components";
 import { aqBootstrapTheme } from "../theme/defaultTheme";
-import { BaseProps } from "../../shared/_types";
 import { ModalProps } from "./_types";
 import { Modal } from "./Modal";
 
@@ -10,10 +9,8 @@ export default {
   component: Modal,
 } as Meta;
 
-const Template: Story<ModalProps & BaseProps> = (args) => (
-  <ThemeProvider theme={aqBootstrapTheme}>
-    <Modal {...args}>This is a modal content</Modal>
-  </ThemeProvider>
+const Template: Story<ModalProps> = (args) => (
+  <Modal {...args}>This is a modal content</Modal>
 );
 
 export const SimpleModal = Template.bind({});
