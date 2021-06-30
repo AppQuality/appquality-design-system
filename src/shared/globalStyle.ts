091495792325c875b5c21a135661e2046b86ac67
iacopolea <iacopo.leardini@app-quality.com>
2021-06-30 14:31:43 +0200

    feat(gllobal): better reset

--- src/shared/globalStyle.ts
+++ src/shared/globalStyle.ts
@@ -19,7 +19,8 @@ const GlobalStyle = createGlobalStyle`${(props) => {
     }
     h1, h2, h3, h4, h5, h6 {
       margin: 0;
-      font-size: unset
+      padding: 0;
+      font: inherit;
     }
     a {
       color: inherit;
