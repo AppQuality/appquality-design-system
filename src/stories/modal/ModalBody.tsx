4ef9cec4df2f5f622962942c186382e1b457107e
Davide Bizzi <davide.bizzi@app-quality.com>
2021-08-27 13:56:02 +0200

    fix(modal): Make modal shorter for phone controls

--- src/stories/modal/ModalBody.tsx
+++ src/stories/modal/ModalBody.tsx
@@ -18,6 +18,6 @@ const BasicModalBody = ({ children, className }: ModalBodyProps) => (
 
 export const ModalBody = styled(BasicModalBody)`
   padding: ${(props) => props.theme.grid.spacing.default};
-  max-height: calc(100vh - 120px);
+  max-height: calc(100vh - 180px);
   overflow: auto;
 `;
