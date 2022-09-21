2c0428452c1e238406c23b02ea7343eca24f330c
alexSp84 <alex1984@hotmail.it>
2022-09-21 17:13:52 +0200

    feat(textarea field): implemented textarea formik field

--- src/index.tsx
+++ src/index.tsx
@@ -51,3 +51,4 @@ export * from "./stories/Steps/Steps";
 export * from "./stories/toastr/Toastr";
 export * from "./stories/placesAutocomplete/PlacesAutocomplete";
 export * from "./stories/dropzone/Dropzone";
+export * from "./stories/form/textareaField/TextareaField";
