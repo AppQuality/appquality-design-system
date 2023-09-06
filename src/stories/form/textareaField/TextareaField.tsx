ff2d2e6dfb26f30ac11b1fcd755c37fd05f4f2a9
Iacopo Leardini <iacopo.lea@gmail.com>
2023-09-06 16:05:10 +0200

    feat(textarea): add counter

--- src/stories/form/textareaField/TextareaField.tsx
+++ src/stories/form/textareaField/TextareaField.tsx
@@ -1,8 +1,9 @@
 import { Field as FormikField, FieldProps } from "formik";
 import { ErrorMessage } from "../Form";
 import FormLabel from "../formlabel/FormLabel";
-import { StyledTextareaField } from "./_styles";
+import { Counter, StyledTextareaField } from "./_styles";
 import { TextareaFieldProps } from "./_types";
+import { useState } from "react";
 
 export const TextareaField = ({
   name,
@@ -13,7 +14,10 @@ export const TextareaField = ({
   resize,
   height,
   autoResize,
+  counterMax,
+  ...props
 }: TextareaFieldProps) => {
+  const [isFocus, setIsFocus] = useState(false);
   return (
     <FormikField name={name}>
       {({ field, meta }: FieldProps) => {
@@ -24,26 +28,41 @@ export const TextareaField = ({
             isInvalid={meta.touched && typeof meta.error == "string"}
             height={height}
             autoResize={autoResize}
+            {...props}
           >
             {label && <FormLabel htmlFor={field.name} label={label} />}
-            <textarea
-              id={field.name}
-              name={field.name}
-              value={field.value}
-              placeholder={placeholder}
-              disabled={disabled}
-              onChange={(e) => field.onChange(e)}
-              onBlur={(e) => field.onBlur(e)}
-              onInput={
-                autoResize
-                  ? (e) => {
-                      e.currentTarget.style.height = "1px";
-                      e.currentTarget.style.height =
-                        e.currentTarget.scrollHeight + "px";
-                    }
-                  : undefined
-              }
-            />
+            <div style={{ position: "relative" }}>
+              <textarea
+                id={field.name}
+                name={field.name}
+                value={field.value}
+                placeholder={placeholder}
+                disabled={disabled}
+                onChange={(e) => field.onChange(e)}
+                onBlur={(e) => {
+                  field.onBlur(e);
+                  setIsFocus(false);
+                }}
+                onFocus={() => setIsFocus(true)}
+                onInput={
+                  autoResize
+                    ? (e) => {
+                        e.currentTarget.style.height = "1px";
+                        e.currentTarget.style.height =
+                          e.currentTarget.scrollHeight + "px";
+                      }
+                    : undefined
+                }
+              />
+              {counterMax && (
+                <Counter
+                  isFocus={isFocus}
+                  isInvalid={meta.touched && typeof meta.error == "string"}
+                >
+                  <span>{field.value.length}</span>/{counterMax}
+                </Counter>
+              )}
+            </div>
             <ErrorMessage name={field.name} />
           </StyledTextareaField>
         );
