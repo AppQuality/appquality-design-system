14473421510e9e4eb01c2cec162b491fa6a39331
Iacopo Leardini <iacopo.lea@gmail.com>
2024-02-12 11:23:01 +0100

    feat: use default input date

--- src/stories/DateInput/DateInput.stories.tsx
+++ src/stories/DateInput/DateInput.stories.tsx
@@ -1,19 +1,52 @@
 import { DateInput } from ".";
 import { Story, Meta } from "@storybook/react";
 import { DatepickerGlobalStyle } from "./_style";
+import { useState } from "react";
+import { ErrorMessage } from "formik";
+import { ErrorMessageWrapper } from "../form/Form";
 
 export default {
   title: "Inputs/DateInput",
   component: DateInput,
 } as Meta;
 
-const DatepickerTemplate: Story = (args) => (
-  <>
-    <DatepickerGlobalStyle />
-    <h2>Seleziona una data nel formato gg-mm-aaaa</h2>
-    <DateInput {...args} id="test1" i18n={{ placeholder: "28-03-2013" }} />
-  </>
-);
+const DatepickerTemplate: Story = (args) => {
+  function getEighteenYearsAgo(): string {
+    const date = new Date();
+    date.setFullYear(date.getFullYear() - 18);
+
+    const year = date.getFullYear();
+    const month = ("0" + (date.getMonth() + 1)).slice(-2); // Months are 0 based, so +1 and pad with 0
+    const day = ("0" + date.getDate()).slice(-2);
+
+    return `${year}-${month}-${day}`;
+  }
+  const [value, setValue] = useState("");
+  const [isInvalid, setIsInvalid] = useState(false);
+  return (
+    <>
+      <DatepickerGlobalStyle />
+      <label htmlFor="test1">
+        Birthdate - min 18 years old (example events in console log)
+      </label>
+      <DateInput
+        {...args}
+        isInvalid={isInvalid}
+        id="test1"
+        value={value}
+        max={getEighteenYearsAgo()}
+        onChange={(e) => setValue(e.target.value)}
+        onBlur={(e) => {
+          setIsInvalid(!value);
+        }}
+      />
+      {isInvalid && (
+        <ErrorMessageWrapper>birth date is mandatory</ErrorMessageWrapper>
+      )}
+      value: {value}
+    </>
+  );
+};
 export const DatepickerInput = DatepickerTemplate.bind({});
 DatepickerInput.args = {
   onChange: ({ value }: { value: Date }) => {
--- src/stories/DateInput/index.tsx
+++ src/stories/DateInput/index.tsx
@@ -20,122 +20,14 @@ const StyledInput = styled(FormInput)`
     padding: 0px;
   }
 `;
-export interface DatepickerProps {
-  id: string;
-  name?: string;
-  value?: string;
-  isInvalid?: boolean;
-  minDate?: Date;
-  maxDate?: Date;
-  onOpen?: () => void;
-  onCancel?: ({ value, valueText }: { value: Date; valueText: string }) => void;
-  onChange?: ChangeEventHandler<HTMLInputElement>;
-  controls?: Array<"date" | "calendar">;
-  inputProps?: React.InputHTMLAttributes<HTMLInputElement>;
-  i18n?: {
-    locale?: string;
-    dateFormat?: string;
-    placeholder?: string;
-    setText?: string;
-    cancelText?: string;
-    buttonTitle?: string;
-  };
-}
 
 export const DateInput = ({
-  id,
-  name,
-  value,
-  minDate,
-  maxDate,
-  onChange,
-  onOpen,
-  onCancel,
-  controls = ["date"],
-  inputProps,
   isInvalid,
-  i18n: {
-    locale = "en",
-    placeholder,
-    setText = "Set",
-    cancelText = "Cancel",
-    dateFormat = "DD-MM-YYYY",
-    buttonTitle = "Open datepicker",
-  } = {},
-}: DatepickerProps) => {
-  let currentLocale = localeEn;
-  const ref = React.useRef<HTMLInputElement>(null);
-  if (locale === "it") currentLocale = localeIt;
-  if (locale === "es") currentLocale = localeEs;
-  const [openPicker, setOpenPicker] = useState(false);
-  const [inputValue, setInputValue] = useState<string>(value || "");
-
-  const show = () => {
-    setOpenPicker(true);
-  };
-  const onClose = () => {
-    setOpenPicker(false);
-  };
-  const onDatePick = (v: { value: Date; valueText: string }) => {
-    setInputValue(v.valueText);
-    if (ref.current) {
-      ref.current.value = v.valueText;
-      ref.current.dispatchEvent(new Event("input", { bubbles: true }));
-    }
-    onClose();
-  };
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value);
-    onChange && onChange(e);
-  };
+  ...props
+}: { isInvalid?: boolean } & React.InputHTMLAttributes<HTMLInputElement>) => {
   return (
     <StyledInput type="text" isInvalid={isInvalid}>
-      <input
-        id={id}
-        name={name}
-        type="text"
-        value={inputValue}
-        onInput={handleInputChange}
-        placeholder={placeholder}
-        ref={ref}
-        {...inputProps}
-      />
-      <MobiScrollDatePicker
-        select="date"
-        min={minDate}
-        max={maxDate}
-        locale={currentLocale}
-        onOpen={onOpen}
-        onCancel={onCancel}
-        onChange={onDatePick}
-        placeholder={placeholder}
-        dateFormat={dateFormat}
-        setText={setText}
-        cancelText={cancelText}
-        buttons={["cancel", "set"]}
-        theme="appquality"
-        controls={controls}
-        themeVariant="light"
-        showOnFocus={false}
-        showOnClick={false}
-        isOpen={openPicker}
-        onClose={onClose}
-        inputProps={{
-          "aria-hidden": "true",
-          tabIndex: -1,
-        }}
-      />
-      <span className="input-group-button">
-        <Button
-          title={buttonTitle}
-          size="sm"
-          type="button"
-          kind="transparent"
-          onClick={show}
-        >
-          <CalendarIcon color={aqBootstrapTheme.variants.primary} />
-        </Button>
-      </span>
+      <input id={props.id} name={props.name} type="date" {...props} />
     </StyledInput>
   );
 };
