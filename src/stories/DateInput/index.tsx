c22b478430c2decbbe8289acd2dcebd79f06d24c
Davide Bizzi <davide.bizzi@unguess.io>
2024-04-03 10:50:59 +0200

    fix: Pad format date

--- src/stories/DateInput/index.tsx
+++ src/stories/DateInput/index.tsx
@@ -11,7 +11,7 @@ const StyledInput = styled(FormInput)`
   }
 `;
 function formatDate(date: Date): string {
-  const year = date.getFullYear();
+  const year = String(date.getFullYear()).padStart(4, "0");
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const day = String(date.getDate()).padStart(2, "0");
   return `${year}-${month}-${day}`;
--- src/stories/form/datepicker/Datepicker.tsx
+++ src/stories/form/datepicker/Datepicker.tsx
@@ -22,7 +22,7 @@ function parseDate(time: string): Date {
 }
 
 function formatDate(date: Date): string {
-  const year = date.getFullYear();
+  const year = String(date.getFullYear()).padStart(4, "0");
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const day = String(date.getDate()).padStart(2, "0");
   return `${year}-${month}-${day}`;
