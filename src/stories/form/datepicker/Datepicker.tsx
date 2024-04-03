ab550109148d8d9e2479966076d5773f070f15fc
Davide Bizzi <davide.bizzi@unguess.io>
2024-04-03 14:35:00 +0200

    fix: Correct format date

--- src/stories/form/datepicker/Datepicker.tsx
+++ src/stories/form/datepicker/Datepicker.tsx
@@ -23,7 +23,7 @@ function parseDate(time: string): Date {
 
 function formatDate(date: Date): string {
   const year = String(date.getFullYear()).padStart(4, "0");
-  const month = String(date.getMonth()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
   const day = String(date.getDate()).padStart(2, "0");
   return `${year}-${month}-${day}`;
 }
