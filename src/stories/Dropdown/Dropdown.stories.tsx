54f93e78da5cfea50be9af53eba8a7cf802c9652
Iacopo Leardini <iacopo.lea@gmail.com>
2024-04-18 18:46:53 +0200

    feat(dropdown): Dropdown component and stories

--- src/stories/Dropdown/Dropdown.stories.tsx
+++ src/stories/Dropdown/Dropdown.stories.tsx
@@ -1,16 +1,29 @@
 import { Meta, StoryFn } from "@storybook/react";
-import { SelectNew } from ".";
+import Dropdown, { DropdownProps } from ".";
+import { colourOptions } from "./data";
 
 export default {
-  title: "SelectNew",
-  component: SelectNew,
+  title: "Dropdown",
+  component: Dropdown,
 } as Meta;
 
-const Template: StoryFn<SelectProps> = (args) => {
-  return <SelectNew {...args} />;
+const Template: StoryFn<DropdownProps> = (args) => {
+  return <Dropdown {...args} />;
 };
 
-export const SelectBase = Template.bind({});
-SelectBase.args = {
-  value: { label: "", value: "" },
+export const SingleDropdown = Template.bind({});
+SingleDropdown.args = {
+  className: "basic-single",
+  defaultValue: colourOptions[0],
+  name: "color",
+  options: colourOptions,
+};
+
+export const MultiDropdown = Template.bind({});
+MultiDropdown.args = {
+  isMulti: true,
+  className: "basic-multi",
+  defaultValue: [colourOptions[2], colourOptions[3]],
+  name: "colors",
+  options: colourOptions,
 };
